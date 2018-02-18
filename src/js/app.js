2ae4b76f9524ef0dffb174e44226687f7dfd7e60
Veerappan Saravanan <veerappa@buffalo.edu>
2018-02-18 16:45:12 -0500

    updated to latest UI

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,8 @@ App = {
   contracts: {},
   names: new Array(),
   url: 'http://127.0.0.1:9545',
+  chairPerson:null,
+  currentAccount:null,
   init: function() {
     // Load CANDIDATES
     $.getJSON('../proposals.json', function(data) {
@@ -48,57 +50,54 @@ App = {
     App.contracts.vote.setProvider(App.web3Provider);
 
     // Use our contract to retrieve and mark the voted pets
-    // return App.markvoted();     // TOD
+    
+    App.getChairperson();
     return App.bindEvents();
   });
-    //return App.bindEvents();
   },
 
-  /*deploy : function(){
-    App.contracts.info.deployed().then(function(instance) {
-      App.contracts.instance = instance;
-      console.log(App.contracts.instance);
-       App.bindEvents();
-    });
-  },*/
+  
 
   bindEvents: function() {
-    $(document).on('click', '.btn-vote', App.handlevote);
+    $(document).on('click', '.btn-vote', App.handleVote);
     $(document).on('click', '#win-count', App.declareWinner);
-    $(document).on('click', '#register', function(){ var ad = $('#enter_address').val(); console.log(ad); App.handleregister(ad);   });
+    $(document).on('click', '#register', function(){ var ad = $('#enter_address').val(); console.log(ad); App.handleRegister(ad);   });
   },
 
-  // markvoted: function(voters, account) {
-  //   var voteInstance;
-  //   console.log(voteInstance);
-  //   App.contracts.vote.deployed().then(function(instance) {
-  //     voteInstance = instance;
-  //     console.log(voteInstance.getCount.call());
-  //     return voteInstance.getCount.call();
-  //   }).then(function(proposals) {
-  //     for ( i = 0; i < proposals.length ; i++) {
-  //         console.log('hello');
-  //         //$('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
-  //         //$('.panel-proposal').eq(i).find('#vote-count').text(proposals[i] + " ");
-  //
-  //
-  //
-  //     }
-  //   }).catch(function(err) {
-  //     console.log(err.message);
-  //   });
-  //     },
+  
 populateAddress : function(){
  new Web3(new Web3.providers.HttpProvider(App.url)).eth.getAccounts((err, accounts) => {
   console.log(typeof(accounts));
   jQuery.each(accounts,function(i){
-    var optionElement = '<option value="'+accounts[i]+'">'+accounts[i]+'</option';
-    jQuery('#enter_address').append(optionElement);
+    if(web3.eth.coinbase != accounts[i]){
+      var optionElement = '<option value="'+accounts[i]+'">'+accounts[i]+'</option';
+      jQuery('#enter_address').append(optionElement);  
+    }
+    
   });
 
   });
 },
-handleregister: function(addr){
+
+getChairperson : function(){
+  App.contracts.vote.deployed().then(function(instance) {
+      return instance.chairperson();
+    }).then(function(result) {
+      App.chairPerson = result.toString();
+      console.log(result);
+      App.currentAccount = web3.eth.coinbase;
+      if(App.chairPerson != App.currentAccount){
+        jQuery('#address_div').css('display','none');
+        jQuery('#register_div').css('display','none');
+      }else{
+        console.log("asdadad");
+        jQuery('#address_div').css('display','block');
+        jQuery('#register_div').css('display','block');
+      }
+  })
+},
+
+handleRegister: function(addr){
 
     var voteInstance;
     console.log("entered address is:" + addr);
@@ -116,19 +115,11 @@ handleregister: function(addr){
       alert(addr + " account registeration failed")
     })
 
-    //event.preventDefault();
-    /*App.contracts.instance.register(addr).then( function(result){
-      if(result.receipt.status == '0x01')
-        alert(addr + " is registered successfully")
-      else
-        alert(addr + " is not registered successfully")
-    }).catch( function(err){
-      console.log(err.message);
-    })*/
+    
 },
 
 // handling the vote
-  handlevote: function(event) {
+  handleVote: function(event) {
     event.preventDefault();
 
     var proposalId = parseInt($(event.target).data('id'));
@@ -151,30 +142,10 @@ handleregister: function(addr){
         }).catch(function(err){
           alert(account + " voting failed")
         });
-      // .then(function(result) {
-      //   return App.markvoted();
-      // }).catch(function(err) {
-      //   console.log(err.message);
-      // });
+      
 
     });
-    /*event.preventDefault();
-
-    var proposalId = parseInt($(event.target).data('id'));
-    web3.eth.getAccounts(function(error, accounts) {
-      if (error) {
-        console.log(error);
-      }
-
-      var account = accounts[0];
-      App.contracts.instance.vote(proposalId, {from: account}).then(function(result) {
-        if(result.receipt.status == '0x01')
-          alert(account + " voting done successfully")
-        else
-          alert(account + " voting not done successfully")
-      })
-
-    });*/
+    
   },
 
 
@@ -195,11 +166,7 @@ handleregister: function(addr){
     }).catch(function(err){
       console.log(err.message);
     })
-    /*App.contracts.instance.winningProposal().then(function(result){
-      alert(App.names[result] + "  is the winner ! :)");
-    }).catch(function(err){
-      console.log(err.message);
-    })*/
+    
   }
 };
 
